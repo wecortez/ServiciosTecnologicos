55534426dd4150065b15275e0ac5d4a4b5ce9bf1
wecortez <williamesteban1727@gmail.com>
2022-02-14 14:51:08 -0500

    Version1 de proyecto de servicios tecnológicos

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#slider').nivoSlider({
+        effect: 'random',
+        slices: 15,
+        boxCols: 8,
+        boxRows: 4,
+        animSpeed: 500,
+        pauseTime: 3000,
+        startSlide: 0,
+        directionNav: true,
+        controlNav: true,
+        controlNavThumbs: false,
+        pauseOnHover: true,
+        manualAdvance: false,
+        prevText: 'Prev',
+        nextText: 'Next',
+        randomStart: false,
+        beforeChange: function () { },
+        afterChange: function () { },
+        slideshowEnd: function () { },
+        lastSlide: function () { },
+        afterLoad: function () { }
+    });
+
+});
